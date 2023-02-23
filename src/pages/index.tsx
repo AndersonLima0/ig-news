c9eb80d7c172e60668450f3f13059e847502b53b
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-02-23 17:38:48 -0300

    Consumindo a api do Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Head from "next/head";
+//SSR(Server-side Rendering)  SSG(Static Site Generation)
+import { GetServerSideProps, GetStaticProps } from "next";
 
 import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: IHomeProps) {
+  //Clientside
+  //Server-side Rendering
+  //Static Site Generation
   return (
     <>
       <Head>
@@ -18,13 +31,32 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month </span>
+            <span>for {product.amount} month </span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Mek3uKfhy9E7GzpzsIS960M");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100), //o preço unitario vem em centavos
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
