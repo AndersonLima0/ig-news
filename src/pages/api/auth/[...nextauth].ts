d52b8f8676e70fb9b1cf1b41007e0259515e406a
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-02-26 23:28:47 -0300

    Salvando dados do usuário no FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -7,12 +10,30 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
+        url: "https://github.com/login/oauth/authorize",
         params: {
-          scope: "read-user",
+          scope: "read-user user:email",
         },
       },
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const emailDev = user.email;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: { emailDev },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 export default NextAuth(authOptions);
