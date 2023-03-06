e66b5a6034c7a9e535ab65e4f5648573f47c1c56
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-03-06 14:56:52 -0300

    Ativando rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import Link from "next/link";
+import {useRouter} from 'next/router'
 
 export function Header() {
+  const {asPath} = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">
+          <Link className={asPath === '/' ? styles.active : ''} href="/">
             Home
-          </a>
-          <a href="">Posts</a>
+          </Link>
+          <Link className={asPath === '/posts' ? styles.active : ''} href="/posts">
+            Posts
+          </Link>
         </nav>
         <SignInButton />
       </div>
